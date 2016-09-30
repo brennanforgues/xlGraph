3f5ddfe5f06d19ed1276319222ee20fe77f54d00
Forgues,Brennan <brennan.forgues@gmail.com>
2016-09-29 21:53:47 -0400

    adding comments

--- xlGraph.m
+++ xlGraph.m
@@ -9,6 +9,7 @@ xl.Visible = 1;
 
 L = int16.empty;
 
+% chart objects
 xlCO = xlS.ChartObjects.Add(100, 30, 400, 250); 
 xlC = xlCO.Chart;  
 
@@ -17,6 +18,7 @@ if nargin > 10
     error('Too many input arguments to the xlGraph function');
 end
 
+% transpose arrays
 if nargin == 10
     
      if size(x5,1) == 1
@@ -48,13 +50,16 @@ if nargin == 10
      end
      if size(y,1) == 1
          y = y';
-     end        
+     end
+     
+    % Bind input arrays to xl range object
     xlS.Range('A1:B1000').Value = [x y];
     xlS.Range('C1:D1000').Value = [x2 y2];
     xlS.Range('E1:F1000').Value = [x3 y3];
     xlS.Range('G1:H1000').Value = [x4 y4];
     xlS.Range('I1:J1000').Value = [x5 y5];
     
+    % Bind xl range object to xl chart object 
     xlC.SeriesCollection.NewSeries;
     xlC.SeriesCollection(1).Value = xlS.Range('B1:B1000');
     xlC.SeriesCollection(1).XValue = xlS.Range('A1:A1000');
@@ -73,7 +78,8 @@ if nargin == 10
     
 elseif nargin == 9
      error('Must have even number of input arguments to xlGraph');
-     
+
+% transpose arrays
 elseif nargin == 8
      x5 = L;
      y5 = L;
@@ -104,11 +110,13 @@ elseif nargin == 8
          y = y';
      end
         
+     % Bind input arrays to xl range object
      xlS.Range('A1:B1000').Value = [x y];
      xlS.Range('C1:D1000').Value = [x2 y2];
      xlS.Range('E1:F1000').Value = [x3 y3];
      xlS.Range('G1:H1000').Value = [x4 y4];
      
+     % Bind xl range object to xl chart object
      xlC.SeriesCollection.NewSeries;
      xlC.SeriesCollection(1).Value = xlS.Range('B1:B1000');
      xlC.SeriesCollection(1).XValue = xlS.Range('A1:A1000');
@@ -121,8 +129,12 @@ elseif nargin == 8
      xlC.SeriesCollection.NewSeries;
      xlC.SeriesCollection(4).Value = xlS.Range('H1:H1000');
      xlC.SeriesCollection(4).XValue = xlS.Range('G1:G1000');
+
+
 elseif nargin == 7
      error('Must have even number of input arguments to xlGraph');
+     
+% transpose arrays     
 elseif nargin == 6
      x5 = L;
      y5 = L;
@@ -149,10 +161,12 @@ elseif nargin == 6
          y = y';
      end
         
+     % Bind input arrays to xl range object
      xlS.Range('A1:B1000').Value = [x y];
      xlS.Range('C1:D1000').Value = [x2 y2];
      xlS.Range('E1:F1000').Value = [x3 y3];
      
+     % Bind xl range object to xl chart object
      xlC.SeriesCollection.NewSeries;
      xlC.SeriesCollection(1).Value = xlS.Range('B1:B1000');
      xlC.SeriesCollection(1).XValue = xlS.Range('A1:A1000');
@@ -164,6 +178,8 @@ elseif nargin == 6
      xlC.SeriesCollection(3).XValue = xlS.Range('E1:E1000');
 elseif nargin == 5
      error('Must have even number of input arguments to xlGraph');  
+     
+% transpose arrays     
 elseif nargin == 4
      x5 = L;
      y5 = L;
@@ -184,10 +200,12 @@ elseif nargin == 4
      if size(y,1) == 1
          y = y';
      end
-        
+       
+     % Bind input arrays to xl range object
      xlS.Range('A1:B1000').Value = [x y];
      xlS.Range('C1:D1000').Value = [x2 y2];
      
+     % Bind xl range object to xl chart object
      xlC.SeriesCollection.NewSeries;
      xlC.SeriesCollection(1).Value = xlS.Range('B1:B1000');
      xlC.SeriesCollection(1).XValue = xlS.Range('A1:A1000');
@@ -196,6 +214,8 @@ elseif nargin == 4
      xlC.SeriesCollection(2).XValue = xlS.Range('C1:C1000');
 elseif nargin == 3
      error('Must have even number of input arguments to xlGraph');
+     
+% transpose arrays     
 elseif nargin == 2
      x5 = L;
      y5 = L;
@@ -206,6 +226,7 @@ elseif nargin == 2
      x2 = L;
      y2 = L;
      
+     
      if size(x,1) == 1
          x = x';
      end
@@ -214,9 +235,10 @@ elseif nargin == 2
      end
     
      
-        
+     % Bind input arrays to xl range object   
      xlS.Range('A1:B1000').Value = [x y];
      
+     % Bind xl range object to xl chart object
      xlC.SeriesCollection.NewSeries;
      xlC.SeriesCollection(1).Value = xlS.Range('B1:B1000');
      xlC.SeriesCollection(1).XValue = xlS.Range('A1:A1000');
@@ -226,20 +248,16 @@ elseif nargin == 1
      
 end
 
-
+% Set chart type
 xlCO.Chart.ChartType = 1;
 xlCO.Chart.ChartType = 65;
 xlCO.Chart.ChartType = 'xlXYScatterSmooth';
 
+% Set chart title 
 xlCO.Chart.HasTitle = true;
 xlCO.Chart.ChartTitle.Text = 'Figure 1: ';
 
 
-%xlCO.Chart.Axes(xlCategory, xlPrimary).HasTitle = true;
-%xlCO.Chart.Axes(xlCategory, xlPrimary).AxisTitle.Text = 'X Axis';
-
-%xlCO.Chart.Axes(xlValue, xlPrimary).HasTitle = true;
-%xlCO.Chart.Axes(xlValue, xlPrimary).AxisTitle.Text = 'Y Axis';
 
 
 
